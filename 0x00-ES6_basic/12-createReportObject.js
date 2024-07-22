e182e5f4323add6f7fbcd58501c4a0386b010157
Hussein380 <huznigarane@gmail.com>
2024-07-22 18:27:50 +0300

    corrected a typo in the code

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: employeesList, // Using shorthand property notation
-    getNumberOfDepartments(list) { // Using shorthand method notation
-      return Object.keys(list).length;
+    allEmployees: employeesList,
+    getNumberOfDepartments(list) {
+	    return Object.keys(list).length;
     }
   };
 }
