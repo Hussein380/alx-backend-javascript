4ec72e6d98885031987c22daa42d717bd38fd79a
Hussein380 <huznigarane@gmail.com>
2024-09-17 09:25:37 +0300

    corrected the code format

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,11 @@
-function displayMessage(message) {
-	console.log(message);
+/**
+ *Displays message on the STDOUT.
+ @param {string} msg The message to display.
+ */
+
+
+const displayMessage = (msg) => {
+	console.log(msg);
 }
 
 module.exports= displayMessage;
